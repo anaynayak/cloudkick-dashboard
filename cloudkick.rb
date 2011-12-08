d90483d1b47c63ae5aa300845a7a7bdba8fad1e5
Anay Nayak <anayak@thoughtworks.com>
2011-12-08 18:06:56 +0530

    Bare minimum console based cloudkick status

--- cloudkick.rb
+++ cloudkick.rb
@@ -13,7 +13,12 @@ consumer = OAuth::Consumer.new(consumer_key, consumer_secret,
 access_token = OAuth::AccessToken.new(consumer)
 
 # make a signed request!
-req = access_token.get("/1.0/query/nodes")
+req = access_token.get("/2.0/nodes")
+nodes = JSON.parse(req.response.body, :symbolize_names => true)
+mapping =  nodes[:items].collect{ |i| [i[:id],"#{i[:tags].collect{|x| x[:name]}} || #{i[:groups]} ||  #{i[:ipaddress]} || #{i[:details][:instanceId]}"]}
+node_vals = Hash[*mapping.flatten]
+
+req = access_token.get("/2.0/status/nodes?overall_check_statuses=Warning&overall_check_statuses=Error")
 
 case req.response.code
 when "200"
@@ -22,4 +27,10 @@ else
   puts "error: #{req.inspect}"
 end
 
-p checks
+p node_vals
+
+
+checks.each do |id, status| 
+  errors = status[:check_statuses].select{|key, s| s[:status] != 'Ok'}.collect {|key, s| s[:details]}
+  p "Status #{node_vals[id.to_s]} #{errors}"
+end
